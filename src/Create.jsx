36e41bc46cc777cfd7ed6a94bc14f67e864da03a
C <chrismatusemail@gmail.com>
2022-04-13 08:49:40 +0100

    Add logic for API requesting words by tag.

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import { fetchTags } from "./utils/getUtils.js";
+import { fetchTags, fetchWordsByTag } from "./utils/getUtils.js";
 
 class Create extends Component {
   state = {
     tags: null,
     isLoading: null,
     language1: null,
+    err: null,
   };
 
   render() {
     return (
       <div>
         <h1>Create new sentences</h1>
-
+        {this.state.isLoading ? <p>Loading...</p> : <p>Welcome</p>}
         <form
           onChange={(e) => {
             this.setState({ language1: e.target.value });
@@ -29,6 +30,7 @@ class Create extends Component {
             let language1 = this.state.language1;
 
             if (language1) {
+              this.setState({ isLoading: true });
               fetchTags(language1)
                 .then((tags) => {
                   this.setState({ tags, isLoading: false });
@@ -44,7 +46,34 @@ class Create extends Component {
 
         <ul>
           {this.state.tags &&
-            this.state.tags.map((tag) => <li key={tag}>{tag}</li>)}
+            this.state.tags.map((tag) => (
+              <li
+                style={{ color: "purple" }}
+                onClick={() => {
+                  this.setState({ isLoading: true });
+                  fetchWordsByTag(this.state.language1, tag)
+                    .then((words) => {
+                      this.setState({ words, isLoading: false });
+                    })
+                    .catch((err) => this.setState({ err }));
+                }}
+                key={tag}
+              >
+                {tag}
+              </li>
+            ))}
+          {this.state.words &&
+            Object.keys(this.state.words).map((wordSetKey) => (
+              <div key={wordSetKey}>
+                <h2>{wordSetKey}</h2>
+                {this.state.words[wordSetKey].map((wordObj) => (
+                  <div key={wordObj.id}>
+                    <span style={{ color: "darkblue" }}>{wordObj.lemma}</span>
+                    <span style={{ color: "crimson" }}>{wordObj.id}</span>
+                  </div>
+                ))}
+              </div>
+            ))}
         </ul>
       </div>
     );
--- src/utils/getUtils.js
+++ src/utils/getUtils.js
@@ -5,7 +5,7 @@ const baseUrl = "http://localhost:9090/api";
 export const fetchTags = (lang1) => {
   return axios
     .get(
-      `${baseUrl}/educator/tags/${lang1}`
+      `${baseUrl}/educator/tags?language1=${lang1}`
       // ,{headers: { Authorization: `BEARER ${token}` }}
     )
     .then((res) => {
@@ -13,5 +13,19 @@ export const fetchTags = (lang1) => {
     })
     .then((data) => {
       return data["tags"];
+    });
+};
+
+export const fetchWordsByTag = (lang1, tag) => {
+  return axios
+    .get(
+      `${baseUrl}/educator/words?language1=${lang1}&tags=${tag}`
+      // ,{headers: { Authorization: `BEARER ${token}` }}
+    )
+    .then((res) => {
+      return res.data;
+    })
+    .then((data) => {
+      return data["words"];
     });
 };
