279de151afc49b90ecda15097ea94fffca4c5ce2
C <chrismatusemail@gmail.com>
2022-05-13 09:19:40 +0100

    Extract to util for adding and clearing of lines to connect any two elements.

--- src/Play.jsx
+++ src/Play.jsx
@@ -1,72 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./css/Play.module.css";
-
-function adjustLine(from, to, line) {
-  let fT = from.offsetTop + from.offsetHeight / 2;
-  let tT = to.offsetTop + to.offsetHeight / 2;
-  let fL = from.offsetLeft + from.offsetWidth / 2;
-  let tL = to.offsetLeft + to.offsetWidth / 2;
-
-  let CA = Math.abs(tT - fT);
-  let CO = Math.abs(tL - fL);
-  let H = Math.sqrt(CA * CA + CO * CO);
-  let ANG = (180 / Math.PI) * Math.acos(CA / H);
-
-  let top = 0;
-  let left = 0;
-
-  if (tT > fT) {
-    top = (tT - fT) / 2 + fT;
-  } else {
-    top = (fT - tT) / 2 + tT;
-  }
-  if (tL > fL) {
-    left = (tL - fL) / 2 + fL;
-  } else {
-    left = (fL - tL) / 2 + tL;
-  }
-
-  if (
-    (fT < tT && fL < tL) ||
-    (tT < fT && tL < fL) ||
-    (fT > tT && fL > tL) ||
-    (tT > fT && tL > fL)
-  ) {
-    ANG *= -1;
-  }
-  top -= H / 2;
-
-  line.style["-webkit-transform"] = "rotate(" + ANG + "deg)";
-  line.style["-moz-transform"] = "rotate(" + ANG + "deg)";
-  line.style["-ms-transform"] = "rotate(" + ANG + "deg)";
-  line.style["-o-transform"] = "rotate(" + ANG + "deg)";
-  line.style["-transform"] = "rotate(" + ANG + "deg)";
-  line.style.top = top + "px";
-  line.style.left = left + "px";
-  line.style.height = H + "px";
-}
+const diUtils = require("./utils/displayUtils");
 
 const Play = () => {
+  const [lineArray, setLineArray] = useState([]);
   return (
     <div>
       <h1>Welcome, Player!</h1>
       <h2>Begin game?</h2>
       <h1
         onClick={() => {
-          console.log("adjustLine!");
-          adjustLine(
-            document.getElementById("div1"),
-            document.getElementById("div2"),
-            document.getElementById("line")
-          );
+          diUtils.drawLineBetweenElements("apple", "banana", setLineArray);
         }}
       >
-        Image testing...
+        Add lines
       </h1>
-      <div className={styles.holder}>
-        <div id="div1" className={`${styles.mydiv} ${styles.div1}`}></div>
-        <div id="div2" className={`${styles.mydiv} ${styles.div2}`}></div>
-        <div id="line" className={styles.line}></div>
+      <h1
+        onClick={() => {
+          setLineArray([]);
+        }}
+      >
+        Destroy lines
+      </h1>
+      <div id="holder" className={styles.holder}>
+        <div id="apple" className={`${styles.mydiv} ${styles.div1}`}></div>
+        <div id="banana" className={`${styles.mydiv} ${styles.div2}`}></div>
+        {lineArray.map((lineID) => (
+          <div id={lineID} key={lineID} className={styles.line}></div>
+        ))}
       </div>
     </div>
   );
--- src/utils/displayUtils.js
+++ src/utils/displayUtils.js
@@ -1,5 +1,68 @@
 const uUtils = require("./universalUtils.js");
 
+exports.drawLineBetweenElements = (
+  elementOneId,
+  elementTwoId,
+  setLineArray
+) => {
+  let lineId = `${elementOneId}-${elementTwoId}-connectingLine`;
+
+  setLineArray((prev) => {
+    let lineArray = prev.slice(0);
+    lineArray.push(lineId);
+    return lineArray;
+  });
+
+  setTimeout(() => {
+    let line = document.getElementById(lineId);
+    let from = document.getElementById(elementOneId);
+    let to = document.getElementById(elementTwoId);
+
+    let fT = from.offsetTop + from.offsetHeight / 2;
+    let tT = to.offsetTop + to.offsetHeight / 2;
+    let fL = from.offsetLeft + from.offsetWidth / 2;
+    let tL = to.offsetLeft + to.offsetWidth / 2;
+
+    let CA = Math.abs(tT - fT);
+    let CO = Math.abs(tL - fL);
+    let H = Math.sqrt(CA * CA + CO * CO);
+    let ANG = (180 / Math.PI) * Math.acos(CA / H);
+
+    let top = 0;
+    let left = 0;
+
+    if (tT > fT) {
+      top = (tT - fT) / 2 + fT;
+    } else {
+      top = (fT - tT) / 2 + tT;
+    }
+    if (tL > fL) {
+      left = (tL - fL) / 2 + fL;
+    } else {
+      left = (fL - tL) / 2 + tL;
+    }
+
+    if (
+      (fT < tT && fL < tL) ||
+      (tT < fT && tL < fL) ||
+      (fT > tT && fL > tL) ||
+      (tT > fT && tL > fL)
+    ) {
+      ANG *= -1;
+    }
+    top -= H / 2;
+
+    line.style["-webkit-transform"] = "rotate(" + ANG + "deg)";
+    line.style["-moz-transform"] = "rotate(" + ANG + "deg)";
+    line.style["-ms-transform"] = "rotate(" + ANG + "deg)";
+    line.style["-o-transform"] = "rotate(" + ANG + "deg)";
+    line.style["-transform"] = "rotate(" + ANG + "deg)";
+    line.style.top = top + "px";
+    line.style.left = left + "px";
+    line.style.height = H + "px";
+  }, 50);
+};
+
 exports.orderTraitKeys = (stCh) => {
   let orderedTraitKeys = [
     "chunkId",
