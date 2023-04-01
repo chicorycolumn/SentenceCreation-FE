74731e81032e3ef4fdde1e08fee417e6c4fe42bd
C <chrismatusemail@gmail.com>
2023-04-01 16:56:21 +0100

    Add utils.

--- src/utils/universalUtils.js
+++ src/utils/universalUtils.js
@@ -385,3 +385,13 @@ exports.flatten = (arr) => {
   });
   return res;
 };
+
+exports.round = (num) => {
+  return Math.round(num * 1000) / 1000;
+};
+
+exports.getRandomNumberString = (len) => {
+  return Math.random()
+    .toString()
+    .slice(2, len + 2);
+};
